3812c57e5663b65b05ebfdeb504d4660ead60ddf
jft5568 <james@cedartownonline.com>
2016-12-22 19:30:04 -0500

    Coding

---
+++ public/js/app.js
@@ -0,0 +1,87 @@
+﻿var app = angular.module('App', [
+    'ngRoute',
+    'ng.jsoneditor'
+]);
+
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/home', {
+        templateUrl: 'pages/home.html',
+        controller: 'HomeController'
+    });
+
+    $routeProvider.when('/tester', {
+        templateUrl: 'pages/tester.html',
+        controller: 'TesterController'
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: "/home"
+    });
+});
+
+app.controller('HomeController', function ($scope, $http) {
+
+    console.log('Home');
+
+});
+
+app.controller('TesterController', function ($scope, $http) {
+    var json = { "Array": [1, 2, 3], "Boolean": true, "Null": null, "Number": 123, "Object": { "a": "b", "c": "d" }, "String": "Hello World" };
+    console.log('Tester');
+
+    $scope.data = {};
+    $scope.data.action = "Read";
+    $scope.data.key = { "customerId": 10 };
+    $scope.data.item = {};
+    $scope.reply = {};
+    $scope.serviceId = "HS.CUSTOMER";
+    $scope.username = "devweb";
+    $scope.password = "devweb";
+
+
+    $scope.options = { mode: 'tree' };
+
+    $scope.changeData = function () {
+        $scope.obj.data = { foo: 'bar' };
+    };
+    $scope.changeOptions = function () {
+        $scope.options.mode = $scope.options.mode == 'tree' ? 'code' : 'tree';
+    };
+
+    $scope.submitCall = function () {
+
+
+        var headers = 'Basic ' + window.btoa($scope.username + ":" + $scope.password);
+        headers['Content-Type'] = "application/json";
+        var pData = angular.toJson($scope.data);
+        var url = "/Service/" + $scope.serviceId;
+        $http({
+            method: "POST",
+            url: url,
+            headers: headers,
+            withCredentials: true,
+            data: pData
+        }).then(function (resp) {
+
+            $scope.reply = resp.data;
+        },
+            function () {
+                def.reject([
+                    {
+                        error: "Connection Error",
+                        errorCode: 500
+                    }
+                ]);
+            });
+
+    };
+ 
+    $scope.cloneReply = function() {
+        if ($scope.reply) {
+            if ($scope.reply.key) $scope.data.key = $scope.reply.key;
+            if ($scope.reply.item) $scope.data.item = $scope.reply.item;
+        }
+    }
+});
